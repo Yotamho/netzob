09449e8da8c0594636bc4e7a2af3d2a0ec0ecd6d
yotam <yotam.hochman@cyberbit.com>
2018-07-24 00:13:08 -0700

    Complete FieldSplitOffset.split()

--- netzob/src/netzob/Inference/Vocabulary/FormatOperations/FieldSplitOffset.py
+++ netzob/src/netzob/Inference/Vocabulary/FormatOperations/FieldSplitOffset.py
@@ -1,5 +1,9 @@
+import itertools
 from netzob.Common.Utils.Decorators import NetzobLogger, typeCheck
 from netzob.Model.Vocabulary.AbstractField import AbstractField
+from netzob.Model.Vocabulary.Domain.DomainFactory import DomainFactory
+from netzob.Model.Vocabulary.Field import Field
+from netzob.Model.Vocabulary.Types.Raw import Raw
 
 
 @NetzobLogger
@@ -7,9 +11,10 @@ class FieldSplitOffset(object):
 
     @staticmethod
     @typeCheck(AbstractField, list)
-    def split(field, offsets):
+    def split(field: Field, offsets):
         """
 
+        FIXME works only for separating by 2 offsets, otherwise will raise error when parsed by MessageParser.
         :param field:
         :param offsets:
         :return:
@@ -20,7 +25,46 @@ class FieldSplitOffset(object):
 
         for cell in field.getValues(encoded=False, styled=False):
             splitted_messages.append(FieldSplitOffset.split_bytes_by_offsets(cell, offsets))
-        print(splitted_messages)
+        splitted_fields = list(itertools.zip_longest(*splitted_messages))
+        new_fields = []
+        i_field = -1
+        for i in range(len(splitted_fields)):
+            i_field += 1
+
+            field_domain = list()
+
+            # temporary set that hosts all the observed values to prevent useless duplicate ones
+            observed_values = set()
+            has_inserted_empty_value = False
+
+            is_empty_field = True  # To avoid adding an empty field
+            for v in splitted_fields[i]:
+                if v != "" and v is not None:
+                    is_empty_field = False
+
+                    if v not in observed_values:
+                        field_domain.append(Raw(v))
+                        observed_values.add(v)
+                else:
+                    if not has_inserted_empty_value:
+                        field_domain.append(Raw(nbBytes=0))
+                        has_inserted_empty_value = True
+
+            if not is_empty_field:
+                new_field = Field(
+                    domain=DomainFactory.normalizeDomain(field_domain),
+                    name="Field-" + str(i_field))
+                new_field.encodingFunctions = list(
+                    field.encodingFunctions.values())
+                new_fields.append(new_field)
+                i_field += 1
+
+        # Reset the field
+        from netzob.Inference.Vocabulary.Format import Format
+        Format.resetFormat(field)
+
+        # Create a field for each entry
+        field.fields = new_fields
 
     @staticmethod
     def split_bytes_by_offsets(bytes_, offsets):
--- netzob/test/src/test_netzob/suite_Format.py
+++ netzob/test/src/test_netzob/suite_Format.py
@@ -2,7 +2,9 @@ import unittest
 from netzob.Import.PCAPImporter.all import PCAPImporter
 from netzob.Inference.Vocabulary.FormatOperations.FieldSplitOffset import FieldSplitOffset
 from netzob.Inference.Vocabulary.all import Format
+from netzob.Model.Vocabulary.Field import Field
 from netzob.Model.Vocabulary.Symbol import Symbol
+from netzob.Model.Vocabulary.Types.Raw import Raw
 
 
 class TestFormat(unittest.TestCase):
@@ -10,10 +12,35 @@ class TestFormat(unittest.TestCase):
     def test_split_bytes_by_offsets(self):
         self.assertEqual(FieldSplitOffset.split_bytes_by_offsets(b'fgsdfgsdfg', [1, 2, 5]), ["f", "g", "sdf", "dfg"])
 
-    def test_split_offset(self):
+    @staticmethod
+    def test_split_offset():
+        BPF_FILTER = "!(arp) and !(len == 96)"
+
+        messages = PCAPImporter.readFile("/home/research/Downloads/hunter_no_vlan.pcap", nbPackets=500,
+                                         bpfFilter=BPF_FILTER).values()
+        symbol = Symbol(messages=messages)
+        Format.splitOffset(symbol.fields[0], [3, 4])
+        # Format.clusterByKeyField(symbol.fields[0], symbol.fields[0].fields[1])
+        print(symbol)
+
+    @staticmethod
+    def test_split_delimiter():
         BPF_FILTER = "!(arp) and !(len == 96)"
 
         messages = PCAPImporter.readFile("/home/research/Downloads/hunter_no_vlan.pcap", nbPackets=10,
                                          bpfFilter=BPF_FILTER).values()
         symbol = Symbol(messages=messages)
-        Format.splitOffset(symbol.fields[0], [4])
+        Format.splitDelimiter(symbol.fields[0], Raw(b'\05'))
+        print(symbol)
+
+    # @staticmethod
+    # def test_message_parser():
+    #     msg = b'\x07\xbe[\x05\x08,3c\x80\x00\x07\x00d'
+    #     splitted_msg = FieldSplitOffset.split_bytes_by_offsets(msg, [4])
+    #     f0 = Field(domain=Raw(splitted_msg[0]))
+    #     f1 = Field(domain=Raw(splitted_msg[1]))
+    #     print(f0)
+
+TestFormat.test_split_offset()
+# TestFormat.test_split_delimiter()
+# TestFormat.test_message_parser()
