d3a75c31bb77a8dcc321cf4bc86275f8d5f3fc52
yotam <yotam.hochman@cyberbit.com>
2018-07-25 00:43:05 -0700

    Complete Example

--- netzob/test/src/test_netzob/suite_Format.py
+++ netzob/test/src/test_netzob/suite_Format.py
@@ -5,7 +5,9 @@ from netzob.Inference.Vocabulary.EntropyMeasurement import EntropyMeasurement
 from netzob.Inference.Vocabulary.FormatOperations.FieldSplitOffset import FieldSplitOffset
 from netzob.Inference.Vocabulary.RelationFinder import RelationFinder
 from netzob.Inference.Vocabulary.all import Format
+from netzob.Model.Vocabulary.AbstractField import AbstractField
 from netzob.Model.Vocabulary.Domain.Parser.MessageParser import MessageParser
+from netzob.Model.Vocabulary.Domain.Variables.Leafs.Size import Size
 from netzob.Model.Vocabulary.Field import Field
 from netzob.Model.Vocabulary.Messages.AbstractMessage import AbstractMessage
 from netzob.Model.Vocabulary.Symbol import Symbol
@@ -20,15 +22,22 @@ class TestFormat(unittest.TestCase):
     @staticmethod
     def test_split_offset():
         BPF_FILTER = "!(arp) and !(len == 96)"
-        messages = PCAPImporter.readFile("/home/research/Downloads/hunter_no_vlan.pcap", nbPackets=1000,
+        messages = PCAPImporter.readFile("/home/research/Downloads/hunter_no_vlan.pcap", nbPackets=100,
                                          bpfFilter=BPF_FILTER).values()
+
         bytes_entropy = [byte_entropy for byte_entropy in EntropyMeasurement.measure_entropy(messages)]
         print(bytes_entropy)
+
         symbol = Symbol(messages=messages)
+
+        Format.splitStatic(symbol)
+
         Format.splitOffset(symbol, [3, 4, 5])
+
         clusters = Format.clusterByKeyField(symbol, symbol.fields[1])
-        # for key, value in clusters.items():
-        #     print(value)
+        for key, value in clusters.items():
+            print(value)
+
         new_symbol = list(clusters.items())[-1][1]
         print(new_symbol)
         rels = RelationFinder.findOnSymbol(new_symbol)
@@ -41,6 +50,11 @@ class TestFormat(unittest.TestCase):
             print("    " + str('-'.join([f.name for f in rel["y_fields"]])))
             p = [v.getValues()[:] for v in rel["y_fields"]]
             print("    " + str(p))
+            rel["x_fields"][0].domain = Size(rel["y_fields"], factor=1/8.0)
+        print(new_symbol.fields[2].domain)
+
+        message_to_verify = messages[1]
+        print(AbstractField.abstract(message_to_verify.data, list(clusters.values())))
 
 
     @staticmethod
